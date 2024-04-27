e3961a66570b4f08846d92e0f24f15fc510cb53c
zahnatom <47169813+zahnatom@users.noreply.github.com>
2024-04-27 19:05:08 +0200

    removed unnecessary code

--- zahnatoms pack/Database/Vehicles/Oerlikon_RGMK3.lua
+++ zahnatoms pack/Database/Vehicles/Oerlikon_RGMK3.lua
@@ -43,48 +43,6 @@ GT.WS.radar_rotation_type = 0
 -- GT.WS.searchRadarFrequencies = {{8.0e6, 12.0e6}, {12.0e6,18.2e6}}
 GT.WS.fire_on_march = false
 
--- ============================ Phazotron 1RS2-1 TR ==============================================
-
---GT.WS[1-2].LN[1]
--- ws = GT_t.inc_ws()
--- local first_PANTSIR_tracker_id = ws
--- GT.WS[ws] = {
--- 	center = 'Sensor.1',
---     omegaY = 2,
---     omegaZ = 2,
---     pidY = {p=100, i=0.05, d=12, inn = 50},
---     pidZ = {p=100, i=0.05, d=12, inn = 50},
---     angles = { {math.rad(180), math.rad(-180), math.rad(-90), math.rad(80)} },
---     drawArgument1 = 50,
---     drawArgument2 = 51,
---     LN = {
---         [1] = {
---             type = 102,
---             distanceMin = 0,
---             distanceMax = 30000,
---             reactionTime = 1.0,
---             reflection_limit = 0.03,
--- 			frequencyRange = {{8.0e6, 12.0e6},{12.0e6, 18.0e6},},
---             ECM_K = 0.0,
---             min_trg_alt = 0,
---             max_trg_alt = 15000,
---             max_number_of_missiles_channels = 0,
---             beamWidth = math.rad(90),
---         }
---     }
--- }
-
--- local PANTSIR_tracker_ws = {{{'self', ws}}}
-
--- for i=2,2 do 
---     ws = GT_t.inc_ws()
---     GT.WS[ws] = {}
---     set_recursive_metatable(GT.WS[ws], GT.WS[first_PANTSIR_tracker_id])
---     table.insert(PANTSIR_tracker_ws, {{'self', ws}})
--- end	
-
--- ========================= Pantsir-S1 turret =====================================================
-
 --GT.WS[3]
 local ws = GT_t.inc_ws();
 GT.WS[ws] = {};
@@ -104,9 +62,7 @@ GT.WS[ws].reference_angle_Z = math.rad(0);
 GT.WS[ws].stabilizer = true;
 GT.WS[ws].laser = true;
 GT.WS[ws].pointer = "Muzzle.1"
-GT.WS[ws].cockpit = {'_1A29/_1A29', {0.1, 0.0, 0.0} }
-
--- =========================== 2A38M 30 mm autocannon  =============================================
+GT.WS[ws].cockpit = {'Gepard/Gepard', {0.1, 0.0, 0.0} }
 
 --GT.WS[3].LN[1]
 __LN = add_launcher(GT.WS[ws], GT_t.LN_t.Oerlikon_RG_MK3);
@@ -116,45 +72,10 @@ __LN.sightMasterMode = 1;
 __LN.sightIndicationMode = 1;
 __LN.depends_on_unit = {{{"MIM104_ANMPQ65"},},{{"MIM104_ANMPQ65A"},},{{"MIM104_LTAMDS"},},{{"Oerlikon_MSU"},},{{"ELM2084_MMR_AD_RT"},},{{"ELM2084_MMR_WLR"},},{{"ELM2084_MMR_AD_SC"},},{{"ELM2084_MINI_MMR_WLR"},},};
 
--- ============================ 1TPP1 EOS ===== =======================================================
-
--- GT.WS[4].LN[1]
-ws = GT_t.inc_ws();
-GT.WS[ws] = {}
-set_recursive_metatable(GT.WS[ws], GT_t.SS_t.VYMPEL_TRACKER[1]);
-GT.WS[ws].reference_angle_Y = math.rad(0)
-GT.WS[ws].reference_angle_Z = math.rad(0)
-GT.WS[ws].center = 'Sensor.1'
-GT.WS[ws].base = 3
-GT.WS[ws].angles = {
-					{math.rad(150), math.rad(-150), math.rad(-10), math.rad(85)},
-					};
-GT.WS[ws].drawArgument1 = 50
-GT.WS[ws].drawArgument2 = 51
-GT.WS[ws].LN[1].reactionTime = 1;
-GT.WS[ws].LN[1].distanceMin = 0;
-GT.WS[ws].LN[1].distanceMax = 20000;
-GT.WS[ws].LN[1].min_trg_alt = 0;
-GT.WS[ws].LN[1].max_trg_alt = 20000;
-ws = GT_t.inc_ws();
-GT.WS[ws] = {}
-set_recursive_metatable(GT.WS[ws], GT_t.SS_t.VYMPEL_TRACKER[2]);
-GT.WS[ws].base = ws-1;
-local visual_tracker_ws1 = {{{"self", ws-1}}, {{"self", ws}}};
-
 GT.Name = "OerlikonRGMK3"
 GT.DisplayName = _("Oerlikon Revolvergun Mk3");
 GT.DisplayNameShort = _("Oerlikon RG MK3");
 GT.Rate = 25;
-GT.Sensors = {
-    Optic = {
-        "TKN-3B day",
-        "TKN-3B night",
-        "Tunguska optic sight",
-    },
-    Radar = {
-        "2RL80 air",},
-    }
 
 
 GT.EPLRS = true;
